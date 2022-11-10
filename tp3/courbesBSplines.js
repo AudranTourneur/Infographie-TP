e117ad0ffeec801855a90eb3e14fd1a1ea7cba7f
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2022-11-10 15:56:49 +0100

    started tp3

---
+++ tp3/courbesBSplines.js
@@ -0,0 +1,349 @@
+import { updateList } from './gui.js'
+import { Settings } from './input.js'
+import { choose, disposeNode, drawAxisGraduation } from './utils.js'
+
+/*
+  Ce fichier contient la majorité de la logique mathématique pour les courbes de
+  Bézier.
+*/
+
+// Instanciation du renderer (moteur de rendu)
+const renderer =
+    new THREE.WebGLRenderer({ canvas: document.getElementById('canvas') });
+
+
+// Création d'un premier canvas principal
+const canvas = document.getElementById('canvas')
+
+// Comme on a une barre de tâche de 25% à droite, on change la taille de
+// notre renderer
+const desiredWidth = window.innerWidth * 3 / 4
+const desiredHeight = window.innerHeight
+renderer.setSize(desiredWidth, desiredHeight);
+
+// Création de la caméra
+const camera =
+    new THREE.PerspectiveCamera(45, desiredWidth / desiredHeight, 0.5, 1000);
+camera.position.z = 70;
+
+// Instanciation de la scène
+const scene = new THREE.Scene();
+
+
+// On met la couleur du background à vert (valeurs RGB normalisées entre 0 et 1,
+// la composante verte est à 1, les autres à 0)
+scene.background = new THREE.Color(0.3, 0.3, 0.3);
+
+// Objet qui contient les paramètres spécifiés par l'utilisateur
+const settings = new Settings(canvas, camera);
+
+// Définition des matériaux qui seront utilisés plus tard
+const blueMaterial =
+    new THREE.LineBasicMaterial({ color: 0x0000ff, linewidth: 3 });
+const redMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
+const controlMaterial =
+    new THREE.LineBasicMaterial({ color: 0x00ff00, linewidth: 2 })
+
+// Liste des polynômes de Bernstein qu'on dessine dans le fichier polynomes.js
+export const polynomeBernestein = [];
+
+// Utilisée pour ls animations des traits de construction
+let deCasteljauAnimationState = 0;
+let deCasteljauAnimationStateOrder = true;
+
+
+// Données d'initialisation
+// Il s'agit des trois polynômes dans le sujet : c1, c2 et c3
+const c1X = 10  // Offset en X
+const c1Y = 10  // Offset en Y
+const c1S = 10  // Scaling
+let c1 = {
+    data: [
+        { x: c1X + 0 * c1S, y: c1Y + 0 * c1S }, { x: c1X + 0 * c1S, y: c1Y + 1 * c1S },
+        { x: c1X + 1 * c1S, y: c1Y + 1 * c1S }, { x: c1X + 1 * c1S, y: c1Y + 0 * c1S },
+        { x: c1X + 1.5 * c1S, y: c1Y + 1.5 * c1S }, { x: c1X + .5 * c1S, y: c1Y + 3 * c1S },
+    ],
+    visible: true
+}
+
+const c2X = -10
+const c2Y = 10
+const c2S = 10
+let c2 = {
+    data: [
+        { x: c2X + 0 * c2S, y: c2Y + 0 * c2S }, { x: c2X + 1 * c2S, y: c2Y + 0 * c2S },
+        { x: c2X + 0 * c2S, y: c2Y + 1 * c2S }, { x: c2X + 1 * c2S, y: c2Y + 1 * c2S }
+    ],
+    visible: false
+}
+
+
+const c3X = 10
+const c3Y = -10
+const c3S = 10
+let c3 = {
+    data: [
+        { x: c3X + 0 * c3S, y: c3Y + 0 * c3S }, { x: c3X + 1 * c3S, y: c3Y + 1 * c3S },
+        { x: c3X + 0 * c3S, y: c3Y + 1 * c3S }, { x: c3X + 1 * c3S, y: c3Y + 0 * c3S }
+    ],
+    visible: false
+}
+
+// Liste qui contient les structures de contrôle placées par l'utilisateur
+// Par défaut, elle est construite avec c1, c2 et c3
+export let listOfControlStructures = [c1, c2, c3];
+
+function drawPointsBSpline(controlPoints, log) {
+    const step = 0.01;      //pas de t 
+    //console.log(controlPoints)
+
+    let degre = 3;  // Pour l'instant 3 mais c une donne de l'utilisateur
+    let ordre = degre + 1;
+    let vecNoeud = [];
+    let n = controlPoints.length - 1;  // length des points de controles
+    let nbMorceaux = n - ordre + 2;
+    // Noter vecteur de noeud est uniforme peut etre que plus tard on peut le
+    // ettre en entree d'utilisateur
+    for (let i = 0; i <= n + ordre; i++) vecNoeud.push(i);
+    if (log) console.log(vecNoeud);
+    if (log) console.log(controlPoints);
+    if (log) console.log("ordre :", ordre);
+    if (log) console.log("nbmorceaux", nbMorceaux);
+    if (log) console.log("Nbre de points de controle", n);
+    // Renvoie la base N, indice i, exposant m pour un t donné
+    // etape = m
+    function baseBSpline(m, t, i) {
+        if (m == 0) {
+            if (vecNoeud[i] <= t && t < vecNoeud[i + 1])
+                return 1;
+            else
+                return 0;
+        }
+        const valToReturn =
+            ((t - vecNoeud[i]) / (vecNoeud[i + m] - vecNoeud[i])) *
+            baseBSpline(m - 1, t, i) +
+            ((vecNoeud[i + m + 1] - t) / (vecNoeud[i + m + 1] - vecNoeud[i + 1])) *
+            baseBSpline(m - 1, t, i + 1);
+        if (log) {
+            if (!Number.isFinite(valToReturn)) console.log('suspect', valToReturn)
+            //if (valToReturn == 0) console.log('zero')
+        }
+        return valToReturn
+    }
+
+    const colors = [0xFF0000, 0x0000FF, 0x00FFFF, 0xFF00FF, 0xFFFF00]
+
+
+    if (log) console.log("borne inférieure de t :" + vecNoeud[ordre - 1]);
+    if (log) console.log("borne supérieur de t :", vecNoeud[n + 1]);
+    for (let i = 0; i < nbMorceaux; i++) {
+        //for (let i = 0; i < 1; i++) {
+
+        const finalPoints = []  //liste des points finaux de notre courbe de Bspline
+        if (log) console.log('material', i, 'using color', colors[i])
+        const material =
+            new THREE.LineBasicMaterial({ color: colors[i], linewidth: 3 });
+
+        if (log) console.log("-----Pour le morceau n", i);
+        if (log) console.log("On va du point de controle ", i, controlPoints[i], " a ", i + ordre - 1, controlPoints[i + ordre - 1]);
+
+        // ordre k
+        // degré m
+
+        /*
+        for (let t = vecNoeud[ordre - 1]; t < vecNoeud[n + 1]; t += step) {
+            let sumX = 0;
+            let sumY = 0;
+
+            for (let indicePoint = i; indicePoint < i + ordre - 1; indicePoint++) {
+                //console.log('reading n=', n, controlPoints[n])
+                let tmp = baseBSpline(degre, t, indicePoint);
+                //console.log('val de baseBSpline', baseBSpline(degre, t, indicePoint), {degre, t, indicePoint})
+                if (log && tmp == 0) console.log('ça vaut zéro wesh')
+                sumX += controlPoints[indicePoint].x * tmp;
+                sumY += controlPoints[indicePoint].y * tmp;
+            }
+
+            finalPoints.push({ x: sumX, y: sumY })
+
+        }
+        */
+
+        for (let t = vecNoeud[ordre - 1]; t < vecNoeud[n + 1]; t += step) {
+            let sumX = 0;
+            let sumY = 0;
+            for (let i = 0; i < n; i++) {
+                let tmp = baseBSpline(degre, t, i);
+                sumX += controlPoints[i].x * tmp;
+                sumY += controlPoints[i].y * tmp;
+            }
+            finalPoints.push({ x: sumX, y: sumY })
+        }
+
+
+        const newPoints = finalPoints.map(e => new THREE.Vector3(e.x, e.y, 0));
+        const polyGeom = new THREE.BufferGeometry().setFromPoints(newPoints);
+        const curve = new THREE.Line(polyGeom, material);
+
+        scene.add(curve)
+    }
+
+
+}
+
+
+// Etape compris entre 0 et n-1
+// i compris entre 0 et n-m-1
+
+// Dessine notre figure de contrôle
+function drawControlPoints(controlPoints) {
+    const threePoints = controlPoints.map(e => new THREE.Vector3(e.x, e.y, 0));
+    const polyGeom = new THREE.BufferGeometry().setFromPoints(threePoints);
+    const controlPolygon = new THREE.Line(polyGeom, controlMaterial);
+    scene.add(controlPolygon)
+}
+
+
+
+// Applique toutes les transformés, translation, hométhétie et rotation pour un
+// point {x, y} donné
+export function transformPoint(point) {
+    // Paramètres sélectionnés par l'utilisateur
+    const s = settings;
+    // Angle en radian
+    const theta = s.rotationFactorDeg * Math.PI / 180;
+
+    // Translation
+    const translated = { x: point.x + s.translationX, y: point.y + s.translationY }
+
+    // Hométhétie
+    const scaled = {
+        x: translated.x * s.scaleFactor,
+        y: translated.y * s.scaleFactor
+    }
+
+    // Translation vers une nouvelle origine si le centre de rotation n'est pas
+    // (0, 0)
+    const rotationNormalized = {
+        x: scaled.x - s.rotationCenterX,
+        y: scaled.y - s.rotationCenterY
+    }
+
+    // Application d'une matrice de rotation
+    // https://en.wikipedia.org/wiki/Rotation_matrix
+    const rotated = {
+        x: (rotationNormalized.x * Math.cos(theta) -
+            rotationNormalized.y * Math.sin(theta)),
+        y: (rotationNormalized.x * Math.sin(theta) +
+            rotationNormalized.y * Math.cos(theta)),
+    }
+
+    // On ramène le point après avoir effectué une rotation selon une nouvelle
+    // origine
+    const rotatedNormal = {
+        x: rotated.x + s.rotationCenterX,
+        y: rotated.y + s.rotationCenterY,
+    }
+
+    // Coordonnées finales
+    return rotatedNormal;
+}
+
+// Applique la transforme inverse à un point (on l'utilise pour pouvoir ajouter
+// un point lorsque l'utilisateur clique sur le canvas et qu'une transformée est
+// déjà en cours d'application)
+//  Il s'agit de l'exact inverse de la fonction transformPoint(point)
+export function inverseTransformPoint(point) {
+    const s = settings;
+    const theta = s.rotationFactorDeg * Math.PI / 180;
+
+    const rotatedNormal = {
+        x: point.x - s.rotationCenterX,
+        y: point.y - s.rotationCenterY,
+    }
+
+    const rotated = {
+        x: (rotatedNormal.x * Math.cos(theta) + rotatedNormal.y * Math.sin(theta)),
+        y: (rotatedNormal.x * -Math.sin(theta) + rotatedNormal.y * Math.cos(theta)),
+    }
+
+    const rotationNormalized = {
+        x: rotated.x + s.rotationCenterX,
+        y: rotated.y + s.rotationCenterY
+    }
+
+    const scaled = {
+        x: rotationNormalized.x / s.scaleFactor,
+        y: rotationNormalized.y / s.scaleFactor
+    }
+
+    const translated = {
+        x: scaled.x - s.translationX,
+        y: scaled.y - s.translationY
+    }
+
+    return translated;
+}
+
+// Renvoie la liste transformée d'une liste de coordonnés
+function getTransformedList(original) {
+    return original.map(e => { return transformPoint(e) })
+}
+
+// Mise à jour notre canvas
+export function refreshCanvas() {
+    const s = settings;
+
+    // Supprime tous les elements de la scène et supprime de la mémoire les
+    // éléments
+    for (const child of scene.children) {
+        // Si c'est nos axes, on ne les suprime pas
+        if (child == scene.getObjectByName('Axis')) {
+            continue
+        }
+
+        disposeNode(child)
+        scene.remove(child);
+    }
+
+    // On dessine toutes les courbes avec la méthode choisie
+    for (const curve of listOfControlStructures) {
+        if (!curve.visible) {
+            continue;
+        }
+
+        const transformedPoints = getTransformedList(curve.data)
+        drawControlPoints(transformedPoints);
+
+
+    }
+
+    drawPointsBSpline(c1.data, false)
+}
+
+
+drawControlPoints(c1.data)
+drawPointsBSpline(c1.data, true)
+
+
+function animate() {
+    // On demande au naviguateur d'éxecuter cette fonction en continue (60 fois
+    // par seconde en général)
+    requestAnimationFrame(animate);
+    //refreshCanvas();
+
+    // On fait le rendu graphique à chaque frame puisque l'état du monde a
+    // peut-être été modifié
+    renderer.render(scene, camera);
+}
+
+// Première initialisation de notre liste de points à afficher
+updateList(c1)
+
+// On dessine et on créer nos axes
+const axisGroup = drawAxisGraduation();
+axisGroup.name = 'Axis';
+scene.add(axisGroup);
+
+// On appelle la fonction une première fois pour initialiser
+animate();
---
+++ tp3/input.js
@@ -0,0 +1,154 @@
+import { listOfControlStructures } from "./courbesBSplines.js"
+import { handleClick, deleteAllPoints, updateList, addPoint, updateTransformations } from "./gui.js"
+
+/*
+  Ce fichier contient l'ensemble des fonctions relatives à les récupération des paramètres saisies par l'utilisateur dans l'interface à gauche de l'écran.
+  Il s'agit essentiellement d'event listeners attachés à des éléments du DOM.
+*/
+
+
+// Gére les sliders à gauche de l'interface pour les transformations
+function manageInputGroup(domId, object, prop) {
+	const inputBox = document.getElementById(domId)
+	const inputSlider = document.getElementById('slider-' + domId)
+
+	// Event listener pour une input box
+	inputBox.addEventListener('input', event => {
+		object[prop] = Number(event.target.value)
+		inputSlider.value = object[prop]
+		updateList(new Settings().getCurrentlySelectedCurve(listOfControlStructures))
+	})
+
+	// Event listener pour un slider
+	inputSlider.addEventListener('input', event => {
+		object[prop] = Number(event.target.value)
+		inputBox.value = object[prop]
+		updateList(new Settings().getCurrentlySelectedCurve(listOfControlStructures))
+	})
+}
+
+// Classe singleton gérant les paramètres spécifiés par l'utilisateur
+export class Settings {
+	// Translations
+	translationX = 0;
+	translationY = 0;
+
+	// Hométhétie
+	scaleFactor = 1;
+
+	// Rotations
+	rotationFactorDeg = 0;
+	rotationCenterX = 0;
+	rotationCenterY = 0;
+
+	// Algorithme en cours d'utilisation
+	selectedAlgorithm = 'decasteljau'
+
+	// L'animation de De Casteljau est-elle active ?
+	animationDecasteljau = true
+
+	// Onglet actif
+	currentlySelectedTab = 1;
+
+	// Constructeur de la classe dans lequel est déclaré l'ensemble des event listeners
+	constructor(canvas, camera) {
+		// Singleton pattern
+		if (Settings._instance) {
+			return Settings._instance
+		}
+
+		Settings._instance = this;
+
+		// Initialisation des sliders et des inputs
+		manageInputGroup('translation-x', this, 'translationX')
+		manageInputGroup('translation-y', this, 'translationY')
+
+		manageInputGroup('scale-factor', this, 'scaleFactor')
+
+		manageInputGroup('rotation-factor', this, 'rotationFactorDeg')
+		manageInputGroup('rotation-center-x', this, 'rotationCenterX')
+		manageInputGroup('rotation-center-y', this, 'rotationCenterY')
+
+		// Bouton "Remettre à zéro" pour les transformations 
+		document.getElementById('reset-transformation').addEventListener('click', () => {
+			this.translationX = 0;
+			this.translationY = 0;
+			this.scaleFactor = 1;
+			this.rotationFactorDeg = 0;
+			this.rotationCenterX = 0;
+			this.rotationCenterY = 0;
+			updateTransformations()
+		})
+
+		// Sélection de l'algorithme de Bernstein
+		document.getElementById('div-bernstein').addEventListener('click', () => {
+			this.selectedAlgorithm = 'bernstein'
+			document.getElementById('radio-bernstein').checked = true
+		})
+
+		// Sélection de l'algorithme de De Casteljau
+		document.getElementById('div-decasteljau').addEventListener('click', () => {
+			this.selectedAlgorithm = 'decasteljau'
+			document.getElementById('radio-decastleljau').checked = true
+		})
+
+		// Animation
+		document.getElementById('animation-decastleljau').addEventListener('click', event => {
+			this.animationDecasteljau = event.target.checked
+		})
+
+		// Ajout ou modification de points par un clic sur le canvas
+		document.getElementById('canvas').addEventListener('click', event => {
+			handleClick(event, canvas, camera)
+		})
+
+		// Bouton "Supprimer tous les points"
+		document.getElementById('reset-points').addEventListener('click', () => {
+			deleteAllPoints()
+		})
+
+
+		// Gestion des onglets
+		const tabs = [1, 2, 3]
+		for (const tabId of tabs) {
+			document.getElementById(`tab-${tabId}`).addEventListener('click', () => {
+
+				document.getElementById(`tab-${this.currentlySelectedTab}`).classList.toggle('tab-active')
+				this.currentlySelectedTab = tabId
+				document.getElementById(`tab-${this.currentlySelectedTab}`).classList.toggle('tab-active')
+
+				document.getElementById('title-params').innerText = 'Paramètres de la courbe ' + this.currentlySelectedTab
+
+				document.getElementById('is-curve-visible').checked = listOfControlStructures[this.currentlySelectedTab - 1].visible
+
+				updateList(this.getCurrentlySelectedCurve(listOfControlStructures));
+			})
+		}
+
+		// Toggle pour afficher ou cacher une courbe
+		document.getElementById('is-curve-visible').addEventListener('change', e => {
+			const selectedObject = listOfControlStructures[this.currentlySelectedTab - 1]
+			selectedObject.visible = e.target.checked
+		})
+
+		// Ajout d'un point
+		document.getElementById('input-add').addEventListener('click', event => {
+			const x = Number(document.getElementById('input-x').value) || 0
+			const y = Number(document.getElementById('input-y').value) || 0
+			addPoint({ x, y })
+			updateList(this.getCurrentlySelectedCurve())
+		})
+	}
+
+	// Renvoie les données de la courbe sélectionnée
+	getCurrentlySelectedCurveData(listOfCurves) {
+		const selected = listOfCurves[this.currentlySelectedTab - 1]
+		return selected.data
+	}
+
+	// Renvoie la courbe sélectionnée
+	getCurrentlySelectedCurve(listOfCurves) {
+		const selected = listOfCurves[this.currentlySelectedTab - 1]
+		return selected
+	}
+}
